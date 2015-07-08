6eaa5be1941395f2ce989f403bf9b2337b4e55dc
IsaacVH <isaac.vanh@gmail.com>
2015-07-08 14:28:26 -0400

    First Commit - Add PHP
    Uploading PHP + Google MDL (Material Design Lite) for the site.

---
+++ web/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Amigonote</title>
+	</head>
+
+	<body>
+		<header><h1>This is the index page.</h1></header>
+		<main>
+			<div class="container">The main content.<div>
+		</main>
+	</body>
+</html>
